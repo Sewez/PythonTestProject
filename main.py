496acf7806fd98218f3ea9f34bdf35d54147679e
Muhammad Amasha <muhammadamasha@gmail.com>
2016-05-15 16:14:57 +0200

    Adding first 2 classes, employee and company

--- main.py
+++ main.py
@@ -4,45 +4,79 @@ passwords = ["password1", "password2", "password3"]
 
 employees = {"Amasha": 1000, "Ahmed": 2000, "Abass": 3000, "Yehia": 4000, "Gom3a": 5000}
 
-def searchlist(listtosearch, keyword):
-    for index, item in enumerate(listtosearch):
-        if keyword == item:
-            return index
+class employee(object):
+	def __init__(self, emp_name, emp_salary):
+		self.name = emp_name
+		self.salary = emp_salary
+		
+	def salary(self):
+		return self.salary
 
-    return -1
+
+class company(object):
+	def __init__(self, employees):
+		self.staff = employees
+		
+	def get_employee_salary(self, emp_name):
+		for emp in self.staff:
+			if emp.name == emp_name:
+				return emp.salary()
+
+		return -1
+		
+	def __str__(self):
+		return "\n".join(sorted(["Name: %s | Salary: %s " % (x.name, x.salary) for x in self.staff]))
+
+# Create employees objects
+amasha = employee("Amasha", 1000)
+ahmed = employee("Ahmed", 2000)
+osama = employee("Osama", 3000)
+gamal = employee("Gamal", 4000)
+medhat = employee("Medhat", 5000)
+
+a_company = company([amasha, ahmed, osama, gamal, medhat])
+
+print a_company
+
+# def searchlist(listtosearch, keyword):
+    # for index, item in enumerate(listtosearch):
+        # if keyword == item:
+            # return index
+
+    # return -1
 
 
-def searchdic(dictosearch, key):
-    """
-    This function will return the employee salary if found.
-    :param dictosearch:
-    :param key:
-    :return: Employee salary
-    """
-    if key in dictosearch:
-        return dictosearch[key]
+# def searchdic(dictosearch, key):
+    # """
+    # This function will return the employee salary if found.
+    # :param dictosearch:
+    # :param key:
+    # :return: Employee salary
+    # """
+    # if key in dictosearch:
+        # return dictosearch[key]
 
-    return -1
-# Ask user to input his account password
-' Test this comment '
+    # return -1
+# # Ask user to input his account password
+# ' Test this comment '
 
-inputPassword = raw_input("Please enter your password: ")
+# inputPassword = raw_input("Please enter your password: ")
 
-result = searchlist(passwords, inputPassword)
+# result = searchlist(passwords, inputPassword)
 
-'Check if the password is correct'
-if result != -1:
-    print "Password correct."
+# 'Check if the password is correct'
+# if result != -1:
+    # print "Password correct."
 
-    while 1:
-        'Get employee name to search for the salary.'
-        employeename = raw_input("Plese enter employee name: ")
-        result = searchdic(employees, employeename)
+    # while 1:
+        # 'Get employee name to search for the salary.'
+        # employeename = raw_input("Plese enter employee name: ")
+        # result = searchdic(employees, employeename)
 
-        print result
+        # print result
 
-else:
-    print "Password incorrect."
+# else:
+    # print "Password incorrect."
 
 #mylist = [x*x for x in range(3)]
 
